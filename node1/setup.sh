45b004d0f1bae85514ec10462bc0002a0dfb9433
Warren MacEvoy <wmacevoy@gmail.com>
2020-09-07 12:39:51 -0700

    fix setup script

--- node1/setup.sh
+++ node1/setup.sh
@@ -2,12 +2,12 @@
 
 if [ ! -d env ]
 then
-    conda create -py ./env nodejs=8.11.4
+    conda create -y -p ./env nodejs=8.11.4
 fi
 
 if [ ! =x env/bin/mongod ]
 then
-   conda install -py ./env mongodb
+   conda install -y -p ./env mongodb
 fi
 
 
