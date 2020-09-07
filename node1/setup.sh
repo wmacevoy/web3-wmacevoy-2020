d561d26c0055bf4b40e360abf4c983c91184f3a5
Warren MacEvoy <wmacevoy@gmail.com>
2020-09-07 12:30:39 -0700

    modular setup

--- node1/setup.sh
+++ node1/setup.sh
@@ -2,7 +2,12 @@
 
 if [ ! -d env ]
 then
-   conda create -py ./env nodejs=8.11.4
+    conda create -py ./env nodejs=8.11.4
+fi
+
+if [ ! =x env/bin/mongod ]
+then
    conda install -py ./env mongodb
 fi
+
 
