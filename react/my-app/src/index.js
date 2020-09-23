d9324aeb8bfa2cf69e5baf44fbfbd5998fcfe844
Warren MacEvoy <wmacevoy@gmail.com>
2020-09-23 13:55:22 -0700

    part 2

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -3,18 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button 
+                className="square"
+                onClick={() => this.props.onClick()}
+                >
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares : Array(9).fill(null)
+        }
+    }
+
+
     renderSquare(i) {
-        return <Square />;
+        return <Square 
+           value={this.state.squares[i]}
+           onClick={()=>this.handleClick(i)} />;
+    }
+
+    cloneState() {
+        return {squares:this.state.squares.slice()}
+    }
+
+    handleClick(i) {
+        var newState = this.cloneState();
+        newState.squares[i]='X';
+        this.setState(newState);
     }
 
     render() {
